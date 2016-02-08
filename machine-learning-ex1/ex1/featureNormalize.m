1bbce7eab4e03a299d7562ec88900fb5f4d8db33
Michael Spiegle <mike@nauticaltech.com>
2016-02-08 08:36:31 -0500

    Add featureNormalize function
    While it's possible to perform the math via (X - mu) / sigma, it will
    cause broadcast warnings because mu/sigma aren't big enough for X.
    Instead, we call bsxfun() explicitly which will enlarge mu/sigma to the
    needed size.

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -25,6 +25,9 @@ sigma = zeros(1, size(X, 2));
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
+mu = mean(X);
+sigma = std(X);
+X_norm = bsxfun(@rdivide, bsxfun(@minus, X, mu), sigma);
 
 
 
