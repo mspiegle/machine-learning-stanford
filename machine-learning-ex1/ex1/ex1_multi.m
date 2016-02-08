e703e00119a58075ec3807b463cb257f185df2c6
Michael Spiegle <mike@nauticaltech.com>
2016-02-08 08:47:22 -0500

    Experiment with alpha/num_iters, and show price

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -82,8 +82,8 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+alpha = 0.05;
+num_iters = 5000;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -104,7 +104,9 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+G_norm = bsxfun(@rdivide, bsxfun(@minus, [1650 3], mu), sigma);
+G = [1; G_norm'];
+price = theta' * G; % You should change this
 
 
 % ============================================================
@@ -149,7 +151,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+T = [1; 1650; 3];
+price = theta' * T; % You should change this
 
 
 % ============================================================
