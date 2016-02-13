7ea7fde1f14456198a2503119044f741cb8eec91
Michael Spiegle <mike@nauticaltech.com>
2016-02-12 22:10:46 -0500

    Implement plotData()

--- machine-learning-ex2/ex2/plotData.m
+++ machine-learning-ex2/ex2/plotData.m
@@ -1,5 +1,5 @@
 function plotData(X, y)
-%PLOTDATA Plots the data points X and y into a new figure 
+%PLOTDATA Plots the data points X and y into a new figure
 %   PLOTDATA(x,y) plots the data points with + for the positive examples
 %   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
@@ -11,8 +11,10 @@ figure; hold on;
 %               2D plot, using the option 'k+' for the positive
 %               examples and 'ko' for the negative examples.
 %
-
-
+X_pos = X(find(y == 1), :);
+X_neg = X(find(y == 0), :);
+plot(X_pos(:, 1), X_pos(:, 2), "k+", "LineWidth", 2, "MarkerSize", 7);
+plot(X_neg(:, 1), X_neg(:, 2), "ko", "MarkerFaceColor", "y", "MarkerSize", 7);
 
 
 
