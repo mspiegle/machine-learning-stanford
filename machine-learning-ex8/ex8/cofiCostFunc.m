965b8db06fa4bf643a92a0e5cee8d7afc724a9e7
Michael Spiegle <mike@nauticaltech.com>
2016-03-27 16:59:24 -0700

    Implement unregularized gradient

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,9 +40,9 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 J = (1 / 2) * sum(sum(((X * Theta' - Y) .^ 2) .* R));
-
-
-
+error_factor = (X * Theta' - Y) .* R;
+X_grad = error_factor * Theta;
+Theta_grad = error_factor' * X;
 
 
 
